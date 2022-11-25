b36575a91d2d85097410525c580b378fc9d0f872
lin4lins <elina.vl.shramko@gmail.com>
2022-11-25 10:32:18 +0200

    Add user manager

--- djangogramm_pr/signin/models.py
+++ djangogramm_pr/signin/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
+from .managers import UserManager
 # Create your models here.
 
 
@@ -15,6 +16,8 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return f"email:{self.email}"
+
+    users = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
