939d828feb57acb3fd52d31a640d84b10bae3fdd
lin4lins <elina.vl.shramko@gmail.com>
2022-11-25 10:32:50 +0200

    Add functional to show error messages

--- djangogramm_pr/signin/views.py
+++ djangogramm_pr/signin/views.py
@@ -2,10 +2,9 @@ from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.template.loader import render_to_string
-from django.utils.encoding import (DjangoUnicodeDecodeError, force_bytes,
-                                   force_str)
+from django.utils.encoding import (DjangoUnicodeDecodeError, force_bytes, force_str)
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.views import View
 from djgrm_site.settings import EMAIL_HOST_USER
@@ -29,7 +28,8 @@ class SignUpView(View):
             self.__send_confirmation_email(request, user)
             return HttpResponse("Check your email")
 
-        return HttpResponse("Something went wrong")
+        error_messages = [message for message in form.errors.values()]
+        return render(request, 'errors.html', {'error_messages': error_messages})
 
     @staticmethod
     def __send_confirmation_email(request, user) -> None:
@@ -50,7 +50,7 @@ class ConfirmationView(View):
     def get(self, request, uidb64, token):
         try:
             user_id = force_str(urlsafe_base64_decode(uidb64))
-            user = User.objects.get(id=user_id)
+            user = User.users.get(id=user_id)
 
         except DjangoUnicodeDecodeError:
             user = None
@@ -60,4 +60,6 @@ class ConfirmationView(View):
             user.save()
             login(request, user)
 
-        return render(request, "home.html")
+            return render(request, "home.html")
+
+        return HttpResponse("nope")
