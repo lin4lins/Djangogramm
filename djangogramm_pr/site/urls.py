c81fb4b7fde84e844a740d6c862f68482d7eb800
foxstudent103106 <elina.vl.shramko@gmail.com>
2022-11-29 10:52:28 +0200

    Renaming

--- djangogramm_pr/site/urls.py
+++ djangogramm_pr/site/urls.py
@@ -1,4 +1,4 @@
-"""djgrm_site URL Configuration
+"""site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- djangogramm_pr/site/wsgi.py
+++ djangogramm_pr/site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djgrm_site project.
+WSGI config for site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djgrm_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site.settings')
 
 application = get_wsgi_application()
