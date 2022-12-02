c13ea30e929997703b8fecfbee6f74a529e523af
foxstudent103106 <elina.vl.shramko@gmail.com>
2022-12-02 14:01:55 +0200

    Update queries

--- djangogramm_pr/djangogramm/views/profile_views.py
+++ djangogramm_pr/djangogramm/views/profile_views.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.views import View
-from django.views.generic import CreateView, TemplateView
-
 from djangogramm.forms import ProfileForm
 from djangogramm.models import Profile
-
 
 # Create your views here
 
@@ -43,7 +40,7 @@ class ProfileUpdateView(LoginRequiredMixin, View):
     login_url = '/auth/login'
 
     def get(self, request):
-        profile = self.model.objects.filter(user=request.user).first()
+        profile = self.model.objects.get(user=request.user)
         filled_form = self.form(initial={'full_name': profile.full_name,
                                                'bio': profile.bio,
                                                'avatar': profile.avatar})
@@ -53,7 +50,7 @@ class ProfileUpdateView(LoginRequiredMixin, View):
         form = self.form(request.POST, request.FILES)
 
         if form.is_valid():
-            profile = self.model.objects.filter(user=request.user).first()
+            profile = self.model.objects.get(user=request.user)
             profile.full_name = form.cleaned_data['full_name']
             profile.bio = form.cleaned_data['bio']
             profile.avatar = form.cleaned_data['avatar']
