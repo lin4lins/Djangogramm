2c9c3833f7f996bb44ab25f24b3414db4057640b
Elina Shramko <elina.vl.shramko@gmail.com>
2023-01-05 14:23:19 +0200

    Update profile views & template

--- djangogramm_pr/djangogramm/views/profile.py
+++ djangogramm_pr/djangogramm/views/profile.py
@@ -2,7 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.views import View
 from djangogramm.forms import ProfileForm
-from djangogramm.models import Profile
+from djangogramm.models import Profile, Post
+
 
 # Create your views here
 
@@ -12,35 +13,34 @@ class ProfileCreateView(LoginRequiredMixin, View):
     login_url = '/auth/login'
 
     def get(self, request):
-        return render(request, 'djangogramm/profile_create.html', {'form': self.form})
+        return render(request, 'djangogramm/profile_create.html', {'form': ProfileForm})
 
     def post(self, request):
-        form = self.form(request.POST, request.FILES)
+        form = ProfileForm(request.POST, request.FILES)
 
         if form.is_valid():
             profile = form.save(commit=False)
             profile.user = self.request.user
             profile.avatar = form.cleaned_data['avatar']
             profile.save()
-            return redirect(f'/profile/')
+            return redirect('/profile/')
 
 
 class ProfileView(LoginRequiredMixin, View):
-    model = Profile
     login_url = '/auth/login'
 
     def get(self, request):
-        profile = self.model.objects.get(user=request.user)
-        return render(request, 'djangogramm/profile.html', {'profile': profile})
+        profile = Profile.objects.get(user=request.user)
+        posts = Post.objects.filter(author = request.user).order_by('-publication_datetime')
+        return render(request, 'djangogramm/profile.html', {'profile': profile, 'posts': posts})
 
 
 class ProfileUpdateView(LoginRequiredMixin, View):
-    model = Profile
     form = ProfileForm
     login_url = '/auth/login'
 
     def get(self, request):
-        profile = self.model.objects.get(user=request.user)
+        profile = Profile.objects.get(user=request.user)
         filled_form = self.form(initial={'full_name': profile.full_name,
                                                'bio': profile.bio,
                                                'avatar': profile.avatar})
@@ -50,10 +50,10 @@ class ProfileUpdateView(LoginRequiredMixin, View):
         form = self.form(request.POST, request.FILES)
 
         if form.is_valid():
-            profile = self.model.objects.get(user=request.user)
+            profile = Profile.objects.get(user=request.user)
             profile.full_name = form.cleaned_data['full_name']
             profile.bio = form.cleaned_data['bio']
             profile.avatar = form.cleaned_data['avatar']
             profile.save()
-            return redirect(f'/profile/')
+            return redirect('/profile')
 
