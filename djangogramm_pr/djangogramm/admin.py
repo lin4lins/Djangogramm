32e2d49d710f9ec2d2b588af77b66eec8a3f8f30
Elina Shramko <elina.vl.shramko@gmail.com>
2023-01-10 17:10:42 +0200

    Add admin panel

--- djangogramm_pr/djangogramm/admin.py
+++ djangogramm_pr/djangogramm/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from djangogramm.models import Profile, Post, Image, Like, Tag
+
+
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
