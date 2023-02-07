f6abe8c349b7806a0ba57a4338c27b8527495765
Elina Shramko <elina.vl.shramko@gmail.com>
2023-02-07 14:15:10 +0200

    Add display profile photos for profile with none profile photos

--- djangogramm_pr/djangogramm/templatetags/define_follower.py
+++ djangogramm_pr/djangogramm/templatetags/define_follower.py
@@ -6,8 +6,8 @@ register = template.Library()
 
 
 @register.simple_tag
-def is_followed_by_current_profile(current_profile: Profile, profile: Profile, following_profiles: list) -> bool:
-    for follower in following_profiles:
+def is_followed_by_current_profile(current_profile: Profile, profile: Profile, followers: list) -> bool:
+    for follower in followers:
         if follower.who_is_followed == profile and follower.who_follows == current_profile:
             return True
 
--- djangogramm_pr/djangogramm/views/follower.py
+++ djangogramm_pr/djangogramm/views/follower.py
@@ -2,25 +2,33 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views import View
-from djangogramm.models import Profile, Follower
+from djangogramm.mixins import ProfileRequiredMixin
+from djangogramm.models import Follower, Profile
 from signup.models import User
 
 
-class CreateFollowView(LoginRequiredMixin, View):
+class CreateFollowView(LoginRequiredMixin, ProfileRequiredMixin, View):
     login_url = '/auth/login'
 
     def post(self, request, username: str):
         user_to_to_follow = User.objects.get(username=username)
-        profile_to_follow = get_object_or_404(Profile, user=user_to_to_follow)
-        Follower.objects.create(who_follows=request.user.profile, who_is_followed=profile_to_follow)
-        return HttpResponse(status=201)
+        profile_to_follow = Profile.objects.get(user=user_to_to_follow)
+        try:
+            Follower.objects.get(who_follows=request.user.profile, who_is_followed=profile_to_follow)
+
+        except Follower.DoesNotExist:
+            Follower.objects.create(who_follows=request.user.profile, who_is_followed=profile_to_follow)
+            return HttpResponse(status=201)
+
+        else:
+            return HttpResponse(status=404)
 
 
-class DeleteFollowView(LoginRequiredMixin, View):
+class DeleteFollowView(LoginRequiredMixin, ProfileRequiredMixin, View):
     login_url = '/auth/login'
 
     def post(self, request, username: str):
-        user = User.objects.get(username=username)
-        profile_to_unfollow = get_object_or_404(Profile, user=user)
+        user_to_unfollow = User.objects.get(username=username)
+        profile_to_unfollow = Profile.objects.get(user=user_to_unfollow)
         get_object_or_404(Follower, who_follows=request.user.profile, who_is_followed=profile_to_unfollow).delete()
         return HttpResponse(status=204)
