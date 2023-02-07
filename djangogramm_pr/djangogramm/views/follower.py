2f2abfe3071df4814fe0fb5c6ce5fd7c3d9d3753
Elina Shramko <elina.vl.shramko@gmail.com>
2023-02-07 18:24:42 +0200

    Add tests for followers

--- djangogramm_pr/djangogramm/views/follower.py
+++ djangogramm_pr/djangogramm/views/follower.py
@@ -10,14 +10,17 @@ class CreateFollowView(LoginRequiredMixin, View):
     login_url = '/auth/login'
 
     def post(self, request, username: str):
-        user_to_to_follow = User.objects.get(username=username)
-        profile_to_follow = Profile.objects.get(user=user_to_to_follow)
+        user_to_to_follow = get_object_or_404(User, username=username)
+        profile_to_follow = get_object_or_404(Profile, user=user_to_to_follow)
         try:
             Follower.objects.get(who_follows=request.user.profile, who_is_followed=profile_to_follow)
 
         except Follower.DoesNotExist:
             Follower.objects.create(who_follows=request.user.profile, who_is_followed=profile_to_follow)
             return HttpResponse(status=201)
+
+        except User.profile.RelatedObjectDoesNotExist:
+            return HttpResponse(status=404)
 
         else:
             return HttpResponse(status=404)
@@ -27,7 +30,13 @@ class DeleteFollowView(LoginRequiredMixin, View):
     login_url = '/auth/login'
 
     def post(self, request, username: str):
-        user_to_unfollow = User.objects.get(username=username)
-        profile_to_unfollow = Profile.objects.get(user=user_to_unfollow)
-        get_object_or_404(Follower, who_follows=request.user.profile, who_is_followed=profile_to_unfollow).delete()
-        return HttpResponse(status=204)
+        user_to_unfollow = get_object_or_404(User, username=username)
+        profile_to_unfollow = get_object_or_404(Profile, user=user_to_unfollow)
+        try:
+            get_object_or_404(Follower, who_follows=request.user.profile, who_is_followed=profile_to_unfollow).delete()
+
+        except User.profile.RelatedObjectDoesNotExist:
+            return HttpResponse(status=404)
+
+        else:
+            return HttpResponse(status=204)
