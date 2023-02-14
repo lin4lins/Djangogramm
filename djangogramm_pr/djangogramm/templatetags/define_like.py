e62d74feb1eafa915394b7c3f5ebc0895760cc87
Elina Shramko <elina.vl.shramko@gmail.com>
2023-02-14 11:24:20 +0200

    Fix get_like function to check both like profile and post

--- djangogramm_pr/djangogramm/templatetags/define_like.py
+++ djangogramm_pr/djangogramm/templatetags/define_like.py
@@ -1,11 +1,11 @@
 from django import template
 
-from djangogramm.models import Profile
+from djangogramm.models import Profile, Post
 
 register = template.Library()
 
 @register.simple_tag
-def get_like(current_profile: Profile, likes: list):
+def get_like(current_profile: Profile, likes: list, post: Post):
     for like in likes:
-        if like.profile == current_profile:
-            return like
+        if like.profile == current_profile and like.post == post:
+            return True
